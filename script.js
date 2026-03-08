b75e877432527e14eda2596653b4926b126996c5
Amo-tech-lab <thatoa571@gmail.com>
2026-03-08 14:07:18 +0200

    Added back to top button

--- script.js
+++ script.js
@@ -55,3 +55,19 @@ function typeEffect() {
 
 typeEffect();
 
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 20) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+backToTopButton.addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+
